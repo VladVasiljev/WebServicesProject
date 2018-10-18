5d39521fde7ef2d30946bea6b6d44cf4cb198590
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2018-10-18 17:16:45 +0100

    Working WeatherAPI

---
+++ jerseytutorial/src/main/java/com/mycompany/jerseytutorial/Tester.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jerseytutorial;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+public class Tester {
+
+    @GET
+    @Path("/q={city}&mode={mode}")
+    public Response getWeather(@PathParam("city") String city, @PathParam("mode") String mode) {
+        String URL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&mode=" + mode + "&appid=5862c5b7badb7604d908a212edf84826";
+        System.out.println(URL);
+        Client c = ClientBuilder.newClient();
+        Response r = c.target(URL).request().get();
+        return r;
+    }
+}
