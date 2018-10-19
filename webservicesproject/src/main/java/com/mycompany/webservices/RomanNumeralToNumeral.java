e1711b4e8aa114e41c838fa14c39edbc33ec4992
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2018-10-19 11:49:07 +0100

    RomanConveter works from one path

--- webservicesproject/src/main/java/com/mycompany/webservices/RomanNumeralToNumeral.java
+++ webservicesproject/src/main/java/com/mycompany/webservices/RomanNumeralToNumeral.java
@@ -19,18 +19,18 @@ import javax.ws.rs.core.Response;
 @Path("/number") 
 public class RomanNumeralToNumeral {
     
-  @GET
-  @Path("/{param}")
-  @Produces(MediaType.TEXT_HTML)
-  public Response romanNumeralToNumeral(@PathParam("param") String message) {
-    RomanNumeralConverter con = new RomanNumeralConverter();
-        String str = message.toUpperCase();
-        //Integer.parseInt(str);
-        int getResult = con.romanToDecimal(str);
-        String output = "<html><body><h1>Roman Numeral Entered Was " + message + " Converted Roman Numeral to Number is " + getResult + "</h1></body></html>";
-					
-    return Response.status(200).entity(output).build(); 
-  }
+//  @GET
+//  @Path("/{param}")
+//  @Produces(MediaType.TEXT_HTML)
+//  public Response romanNumeralToNumeral(@PathParam("param") String message) {
+//    RomanNumeralConverter con = new RomanNumeralConverter();
+//        String str = message.toUpperCase();
+//        //Integer.parseInt(str);
+//        int getResult = con.romanToDecimal(str);
+//        String output = "<html><body><h1>Roman Numeral Entered Was " + message + " Converted Roman Numeral to Number is " + getResult + "</h1></body></html>";
+//					
+//    return Response.status(200).entity(output).build(); 
+//  }
     }
   
   
